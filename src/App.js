5432f9b3b19de51ec635c94ad5c16c4a29559300
Joe Bender <joebenderweb@gmail.com>
2021-06-29 15:51:00 -0400

    Add Square click handler, remove Row

--- src/App.js
+++ src/App.js
@@ -1,66 +1,31 @@
 import "./App.css";
 
-function getPiece(abbrev) {
-  switch (abbrev) {
-    case "bp":
-      return "\u265F";
-    case "br":
-      return "\u265C";
-    case "bn":
-      return "\u265E";
-    case "bb":
-      return "\u265D";
-    case "bq":
-      return "\u265B";
-    case "bk":
-      return "\u265A";
-    case "wp":
-      return "\u2659";
-    case "wr":
-      return "\u2656";
-    case "wn":
-      return "\u2658";
-    case "wb":
-      return "\u2657";
-    case "wq":
-      return "\u2655";
-    case "wk":
-      return "\u2654";
-    default:
-      return;
-  }
-}
+const getPiece = {
+  bp: "\u265F",
+  br: "\u265C",
+  bn: "\u265E",
+  bb: "\u265D",
+  bq: "\u265B",
+  bk: "\u265A",
+  wp: "\u2659",
+  wr: "\u2656",
+  wn: "\u2658",
+  wb: "\u2657",
+  wq: "\u2655",
+  wk: "\u2654",
+  null: null,
+};
 
 function Square(props) {
-  return <td className={props.color}>{props.piece}</td>;
-}
-
-function Row(props) {
-  if (props.startWhite) {
-    return (
-      <tr>
-        <Square color="white" piece={props.pieces[0]} />
-        <Square color="black" piece={props.pieces[1]} />
-        <Square color="white" piece={props.pieces[2]} />
-        <Square color="black" piece={props.pieces[3]} />
-        <Square color="white" piece={props.pieces[4]} />
-        <Square color="black" piece={props.pieces[5]} />
-        <Square color="white" piece={props.pieces[6]} />
-        <Square color="black" piece={props.pieces[7]} />
-      </tr>
-    );
-  }
   return (
-    <tr>
-      <Square color="black" piece={props.pieces[0]} />
-      <Square color="white" piece={props.pieces[1]} />
-      <Square color="black" piece={props.pieces[2]} />
-      <Square color="white" piece={props.pieces[3]} />
-      <Square color="black" piece={props.pieces[4]} />
-      <Square color="white" piece={props.pieces[5]} />
-      <Square color="black" piece={props.pieces[6]} />
-      <Square color="white" piece={props.pieces[7]} />
-    </tr>
+    <td
+      onClick={props.onClick}
+      data-row={props.row}
+      data-col={props.col}
+      className={props.color}
+    >
+      {props.piece}
+    </td>
   );
 }
 
@@ -76,17 +41,29 @@ function Board() {
     ["wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"],
   ];
 
+  const selectSquare = (e) => {
+    const row = e.target.dataset.row;
+    const col = e.target.dataset.col;
+    console.log(`Row: ${row}, Col: ${col}`);
+  };
+
   return (
     <table>
       <tbody>
-        <Row startWhite={true} pieces={boardState[0].map(getPiece)} />
-        <Row startWhite={false} pieces={boardState[1].map(getPiece)} />
-        <Row startWhite={true} pieces={boardState[2].map(getPiece)} />
-        <Row startWhite={false} pieces={boardState[3].map(getPiece)} />
-        <Row startWhite={true} pieces={boardState[4].map(getPiece)} />
-        <Row startWhite={false} pieces={boardState[5].map(getPiece)} />
-        <Row startWhite={true} pieces={boardState[6].map(getPiece)} />
-        <Row startWhite={false} pieces={boardState[7].map(getPiece)} />
+        {Array.from({ length: 8 }, (v, i) => i).map((row) => (
+          <tr key={row}>
+            {boardState[row].map((piece, col) => (
+              <Square
+                key={col}
+                color={(row + col) % 2 === 0 ? "white" : "black"}
+                row={row}
+                col={col}
+                piece={getPiece[piece]}
+                onClick={selectSquare}
+              />
+            ))}
+          </tr>
+        ))}
       </tbody>
     </table>
   );
