a58e404397ab3ecdb63607ea819e2c0a5ea31b89
Joe Bender <joebenderweb@gmail.com>
2021-06-29 16:57:27 -0400

    Implement click piece moving (no validation)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 
 const getPiece = {
   bp: "\u265F",
@@ -13,7 +14,6 @@ const getPiece = {
   wb: "\u2657",
   wq: "\u2655",
   wk: "\u2654",
-  null: null,
 };
 
 function Square(props) {
@@ -22,7 +22,7 @@ function Square(props) {
       onClick={props.onClick}
       data-row={props.row}
       data-col={props.col}
-      className={props.color}
+      className={props.selected ? "selected" : props.color}
     >
       {props.piece}
     </td>
@@ -30,7 +30,7 @@ function Square(props) {
 }
 
 function Board() {
-  let boardState = [
+  const [board, setBoard] = useState([
     ["br", "bn", "bb", "bq", "bk", "bb", "bn", "br"],
     ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
     [null, null, null, null, null, null, null, null],
@@ -39,33 +39,49 @@ function Board() {
     [null, null, null, null, null, null, null, null],
     ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
     ["wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"],
-  ];
+  ]);
+  const [selected, setSelected] = useState({ row: -1, col: -1 });
 
   const selectSquare = (e) => {
     const row = e.target.dataset.row;
     const col = e.target.dataset.col;
-    console.log(`Row: ${row}, Col: ${col}`);
+    if (selected.row < 0 && selected.col < 0) {
+      setSelected({ row: parseInt(row), col: parseInt(col) });
+    } else {
+      let newBoard = board.slice();
+      let piece = newBoard[selected.row][selected.col];
+      newBoard[selected.row][selected.col] = null;
+      newBoard[row][col] = piece;
+      setBoard(newBoard);
+      setSelected({ row: -1, col: -1 });
+    }
   };
 
   return (
-    <table>
-      <tbody>
-        {Array.from({ length: 8 }, (v, i) => i).map((row) => (
-          <tr key={row}>
-            {boardState[row].map((piece, col) => (
-              <Square
-                key={col}
-                color={(row + col) % 2 === 0 ? "white" : "black"}
-                row={row}
-                col={col}
-                piece={getPiece[piece]}
-                onClick={selectSquare}
-              />
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div>
+      <table>
+        <tbody>
+          {Array.from({ length: 8 }, (v, i) => i).map((row) => (
+            <tr key={row}>
+              {board[row].map((piece, col) => (
+                <Square
+                  key={col}
+                  color={(row + col) % 2 === 0 ? "white" : "black"}
+                  row={row}
+                  col={col}
+                  piece={getPiece[piece]}
+                  onClick={selectSquare}
+                  selected={row === selected.row && col === selected.col}
+                />
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <h1>
+        {selected.row}, {selected.col}
+      </h1>
+    </div>
   );
 }
 
